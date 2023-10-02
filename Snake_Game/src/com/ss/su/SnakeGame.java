34ce49f05fd65af5ed017a523f9b31f632bdd0e3
Ayan Barai <105867399+Thegeekcoder182@users.noreply.github.com>
2023-10-03 03:14:44 +0530

    Add files via upload

---
+++ Snake_Game/src/com/ss/su/SnakeGame.java
@@ -0,0 +1,11 @@
+package com.ss.su;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame();
+
+	}
+
+}
---
+++ Snake_Game/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author ayanb
+ *
+ */
+module Snake_Game {
+	requires java.desktop;
+}
